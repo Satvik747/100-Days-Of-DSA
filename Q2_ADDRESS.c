1ba9422e57bb6f1574ffe62ebd182df5682d85b7
Satvik Arora <satvik.arora8a@gmail.com>
2025-10-01 21:25:51 +0530

    Add files via upload

---
+++ Q2_ADDRESS.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+    printf("A1\nAdarsh Vihar\nDoiwala\nDehradun");
+    return 0;
+}
---
+++ Q3_NAME.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+   int age;
+   char name[100];
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    printf("Enter your name: ");
+    scanf("%s", name);
+    printf("Your name is %s and your age is %d\n", name, age);
+    return 0;
+}
